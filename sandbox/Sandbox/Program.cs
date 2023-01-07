f345d016b36a56f9bd24de409a529fba553e100a
essunombre <alb21001@byui.edu>
2023-01-07 18:33:28 -0500

    Testing

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hola mundito");
+        Console.WriteLine("Hola worldcito!");
+        Console.WriteLine("What is your name?");
+        string name = Console.ReadLine().Trim();
+
+        if (name == null || name.Length == 0)
+        {
+            Console.WriteLine("Perrito escribe algo!");
+            name = Console.ReadLine();
+
+        }
+        else
+        {
+            Console.WriteLine("Hola " + name);
+        }
+
+        string university = "BYUI";
+        Console.WriteLine($"Yo estudio en {university}");
+
+        
     }
 }
