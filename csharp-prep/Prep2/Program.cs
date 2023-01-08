4dd9340e0d753ab50f7761a74e3372e9dc5840f6
essunombre <alb21001@byui.edu>
2023-01-07 19:24:37 -0500

    Prep 2 added

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        // Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("Insert final grade:");
+        string grade = Console.ReadLine().Trim();
+        int number = int.Parse(grade);
+        string final = "";
+
+        if (number >= 90){
+            final = "A";
+        }
+        else if (number >= 80 ){
+            final = "B";
+        }
+        else if (number >= 70){
+            final = "C";
+        }
+        else if (number >= 60){
+            final = "D";
+        }
+        else if (number < 60){
+            final = "F";
+        }
+
+        Console.WriteLine($"Final Grade: {final}.");
+
     }
 }
