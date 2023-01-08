c91b133ad43a6fd450e8203c1982d1bccfa6fd29
essunombre <alb21001@byui.edu>
2023-01-07 21:18:07 -0500

    Adding prep5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+
+        static void DisplayWelcome()
+        {
+            Console.WriteLine("Welcome to the Program!");
+        }
+
+        static string PromptUserName()
+        {
+            Console.WriteLine("Please enter your name:");
+            string name = Console.ReadLine().Trim();
+            return name;
+        }
+        static int PromptUserNumber()
+        {
+            Console.WriteLine("Please enter your favorite number:");
+            string inputNumber = Console.ReadLine().Trim();
+            int number = int.Parse(inputNumber);
+            return number;
+        }
+        static int SquaredNumber(int number)
+        {
+            int result = number * number;
+            return result;
+        }
+        static void DisplayResult(string name, int number)
+        {
+            Console.WriteLine($"Brother {name}, the square of your number is {number}");
+
+        }
+
+        DisplayWelcome();
+        string promptName = PromptUserName();
+        int promptNumber = PromptUserNumber();
+        int squaredNumber = SquaredNumber(promptNumber);
+        DisplayResult(promptName, squaredNumber);
+
     }
 }
