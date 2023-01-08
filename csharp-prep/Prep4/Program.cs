3154bcca7e1bc4bce205c2ae586cea819bf537d7
essunombre <alb21001@byui.edu>
2023-01-07 20:43:14 -0500

    Perp 4 Added

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+
+        List<int> numbers = new List<int>();
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        bool stop = false;
+        while (!stop)
+        {
+            Console.WriteLine("Enter a number: ");
+            string inputNumber = Console.ReadLine().Trim();
+            int number = int.Parse(inputNumber);
+            if (number == 0)
+            { 
+                stop = true;
+                continue; 
+            }
+            numbers.Add(number);
+        }
+        Console.WriteLine("The sum is: " + numbers.Sum());
+        Console.WriteLine("The average is: " + numbers.Average());
+        Console.WriteLine("The largest number is: " + numbers.Max());
+        // foreach (int number in numbers)
+        // {
+        //     Console.WriteLine(number);
+        // }
+
     }
 }
